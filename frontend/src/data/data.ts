882403015b55b7c2b2b71e047cf1e9f3f50253bd
Aditya Patil <patiladitya2108@gmail.com>
2026-03-24 00:02:25 +0000

    remove Demo from navlink

--- frontend/src/data/data.ts
+++ frontend/src/data/data.ts
@@ -8,7 +8,7 @@ import {
 export const navLinks = [
   { href: "#", label: "Home" },
   { href: "#about", label: "About" },
-  { href: "#demo", label: "Demo" },
+  // { href: "#demo", label: "Demo" },
   { href: "#pricing", label: "Pricing" },
   { href: "#testimonials", label: "Testimonials" },
 ];
